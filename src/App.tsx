c26c89feb61bcdbfc5835fe8833cb62160ccf996
walid-it-hack <boykasmith812@gmail.com>
2025-05-21 18:43:02 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+const students = [
+  { name: "Aiden Carter", level: 1, averageScore: 88 },
+  { name: "Bella Thompson", level: 2, averageScore: 91 },
+  { name: "Caleb Johnson", level: 3, averageScore: 84 },
+  { name: "Daisy Nguyen", level: 3, averageScore: 93 },
+  { name: "Ethan Wright", level: 1, averageScore: 76 },
+  { name: "Fiona Lopez", level: 3, averageScore: 89 },
+  { name: "Gavin Smith", level: 2, averageScore: 82 },
+  { name: "Hannah Patel", level: 3, averageScore: 95 },
+  { name: "Isaac Chen", level: 1, averageScore: 79 },
+  { name: "Jasmine Rivera", level: 2, averageScore: 87 }
+];
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
